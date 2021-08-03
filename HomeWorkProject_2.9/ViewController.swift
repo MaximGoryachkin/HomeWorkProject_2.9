7a166eb02e2a84ae77379063639ab4f215b8a3b3
Maxim Goryachkin <maxgoryachkin@mail.ru>
2021-08-03 16:05:28 +0300

    Initial Commit

---
+++ HomeWorkProject_2.9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkProject_2.9
+//
+//  Created by Максим on 03.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
